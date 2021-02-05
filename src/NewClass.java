93cb301e17fcca6fbd846d8e8cca7fb449af8b47
Камилла <camilla@MacBook-Air-Kamilla.local>
2021-02-06 02:00:08 +0800

    first commit

---
+++ src/NewClass.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author camilla
+ */
+public class NewClass {
+    
+}
+public class JavaApplication46 {
+    public static void main(String[] args) {
+        String s= "Explanable";
+        string z=s.replace('a', 'Z').trim().concat("Aa");
+        z.substring(0,2);
+        System.out.println(z);
+    }
+}
+
+        
